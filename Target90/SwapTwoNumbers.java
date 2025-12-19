f6ed14e38accaf815020264f1b14068ac634bb32
LakshmiKanth-g <87889184+LakshmiKanth-g@users.noreply.github.com>
2025-12-19 14:41:58 +0000

    git commit -m "Renamed Java files to have correct lowercase extensions and added SwapTwoNumbers.java"

---
+++ Target90/SwapTwoNumbers.java
@@ -0,0 +1,21 @@
+//4.) Java program to swap two numbers without using third variable
+
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+    public static void main (String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        System.out.println("Before swap: "+num1);
+        System.out.println("Before swap: "+num2);
+        
+        num1=num1+num2;
+        num2=num1-num2;
+        num1=num1-num2;
+        
+        System.out.println("After swap: "+num1);
+        System.out.println("After swap: "+num2);
+    }
+}
